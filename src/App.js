bd9287166f35afdfc25f4f9186f5a0249b3a5e4f
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-09-04 03:03:51 -1100

    Almost done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { FaRegCopy } from 'react-icons/fa'
 import { BsArrowRightShort } from 'react-icons/bs'
 import './input.css'
 const App = () => {
+  const [strength, setStrength] = useState('')
+  const [password, setPassword] = useState('P4$5WOrD!')
+  const [copied, setCopied] = useState(false)
+  const [length, setLength] = useState(0)
+  const [uppercase, setUppercase] = useState(false)
+  const [lowercase, setLowercase] = useState(false)
+  const [number, setNumber] = useState(false)
+  const [symbol, setSymbol] = useState(false)
+
   return (
     <div className='App'>
       <section className='sec-one'>
@@ -10,35 +19,59 @@ const App = () => {
           password generator
         </h5>
         <header className='div-one d-flex mt-3 p-3'>
-          <p className='font-semibold text-lg'>P4$5WOrD!</p>
-          <FaRegCopy className='icon mt-1' />
+          <p className='font-semibold text-lg'>{password}</p>
+          <div>
+            {copied ? (
+              'Copied!'
+            ) : (
+              <FaRegCopy
+                className='icon mt-1'
+                onClick={() => {
+                  if (password.length > 0) {
+                    navigator.clipboard.writeText(password)
+                    setCopied(true)
+                    setInterval(() => {
+                      setCopied(false)
+                    }, 3000)
+                  }
+                }}
+              />
+            )}
+          </div>
         </header>
         <main className='mt-3 p-3 main-one'>
           <div className='d-flex justify-between'>
             <h5 className='font-mono'>character length</h5>
-            <p>0</p>
+            <p>{length}</p>
           </div>
           <div className='mt-2 div-two'>
-            <input type='range' min='1' max='10' className='w-100 inpuut' />
+            <input
+              type='range'
+              min='0'
+              max='20'
+              value={length}
+              onChange={(e) => setLength(e.target.valueAsNumber)}
+              className='w-100 inpuut'
+            />
           </div>
           <div className='mt-2 d-flex'>
-            <input type='checkbox' />
+            <input type='checkbox' onClick={() => setUppercase(!uppercase)} />
             <p className='capitalize font-mono ml-3'>
               include uppercase letters
             </p>
           </div>
           <div className='mt-2 d-flex'>
-            <input type='checkbox' />
+            <input type='checkbox' onClick={() => setLowercase(!lowercase)} />
             <p className='capitalize font-mono ml-3'>
               include lowercase letters
             </p>
           </div>
           <div className='mt-2 d-flex'>
-            <input type='checkbox' />
+            <input type='checkbox' onClick={() => setNumber(!number)} />
             <p className='capitalize font-mono ml-3'>include numbers</p>
           </div>
           <div className='mt-2 d-flex'>
-            <input type='checkbox' />
+            <input type='checkbox' onClick={() => setSymbol(!symbol)} />
             <p className='capitalize font-mono ml-3'>include symbols</p>
           </div>
           <section className='mt-3 font-mono d-flex justify-between p-3 sec-two'>
@@ -46,15 +79,325 @@ const App = () => {
               <p className='uppercase capa'>strength</p>
             </div>
             <div className='d-flex'>
-              <p className='border'>1</p>
-              <p className='border ml-1'>2</p>
-              <p className='border ml-1'>3</p>
-              <p className='border ml-1'>4</p>
+              <p className='mr-2'>
+                {(length > 0 &&
+                  length <= 5 &&
+                  uppercase === true &&
+                  lowercase === false &&
+                  number === false &&
+                  symbol === false) ||
+                (length > 0 &&
+                  length <= 5 &&
+                  uppercase === false &&
+                  lowercase === true &&
+                  number === false &&
+                  symbol === false) ||
+                (length > 0 &&
+                  length <= 5 &&
+                  uppercase === false &&
+                  lowercase === false &&
+                  number === true &&
+                  symbol === false) ||
+                (length > 0 &&
+                  length <= 5 &&
+                  uppercase === false &&
+                  lowercase === false &&
+                  number === false &&
+                  symbol === true)
+                  ? 'Too Weak!'
+                  : (length > 0 &&
+                      length <= 10 &&
+                      uppercase === true &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === false &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === false &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === true)
+                  ? 'weak'
+                  : (length > 0 &&
+                      uppercase === true &&
+                      lowercase === true &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      uppercase === true &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === true &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === true)
+                  ? 'medium'
+                  : length > 0 &&
+                    uppercase === true &&
+                    lowercase === true &&
+                    number === true &&
+                    symbol === true
+                  ? 'strong'
+                  : ''}
+              </p>
+              <p
+                className='border'
+                style={{
+                  backgroundColor:
+                    (length > 0 &&
+                      length <= 5 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === false &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 5 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 5 &&
+                      uppercase === false &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 5 &&
+                      uppercase === false &&
+                      lowercase === false &&
+                      number === false &&
+                      symbol === true)
+                      ? 'red'
+                      : (length > 0 &&
+                          length <= 10 &&
+                          uppercase === true &&
+                          lowercase === true &&
+                          number === false &&
+                          symbol === false) ||
+                        (length > 0 &&
+                          length <= 10 &&
+                          uppercase === false &&
+                          lowercase === false &&
+                          number === true &&
+                          symbol === true) ||
+                        (length > 0 &&
+                          length <= 10 &&
+                          uppercase === false &&
+                          lowercase === true &&
+                          number === true &&
+                          symbol === false) ||
+                        (length > 0 &&
+                          length <= 10 &&
+                          uppercase === true &&
+                          lowercase === false &&
+                          number === false &&
+                          symbol === true) ||
+                        (length > 0 &&
+                          length <= 10 &&
+                          uppercase === true &&
+                          lowercase === false &&
+                          number === true &&
+                          symbol === false) ||
+                        (length > 0 &&
+                          length <= 10 &&
+                          uppercase === false &&
+                          lowercase === true &&
+                          number === false &&
+                          symbol === true)
+                      ? 'orange'
+                      : (length > 0 &&
+                          uppercase === true &&
+                          lowercase === true &&
+                          number === true &&
+                          symbol === false) ||
+                        (length > 0 &&
+                          uppercase === true &&
+                          lowercase === true &&
+                          number === false &&
+                          symbol === true) ||
+                        (length > 0 &&
+                          uppercase === false &&
+                          lowercase === true &&
+                          number === true &&
+                          symbol === true) ||
+                        (length > 0 &&
+                          uppercase === true &&
+                          lowercase === false &&
+                          number === true &&
+                          symbol === true)
+                      ? 'yellow'
+                      : length > 0 &&
+                        uppercase === true &&
+                        lowercase === true &&
+                        number === true &&
+                        symbol === true
+                      ? 'green'
+                      : '',
+                }}
+              >
+                1
+              </p>
+              <p
+                className='border ml-1'
+                style={{
+                  backgroundColor:
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === true &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === false &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === false &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      length <= 10 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === true)
+                      ? 'orange'
+                      : (length > 0 &&
+                          uppercase === true &&
+                          lowercase === true &&
+                          number === true &&
+                          symbol === false) ||
+                        (length > 0 &&
+                          uppercase === true &&
+                          lowercase === true &&
+                          number === false &&
+                          symbol === true) ||
+                        (length > 0 &&
+                          uppercase === false &&
+                          lowercase === true &&
+                          number === true &&
+                          symbol === true) ||
+                        (length > 0 &&
+                          uppercase === true &&
+                          lowercase === false &&
+                          number === true &&
+                          symbol === true)
+                      ? 'yellow'
+                      : length > 0 &&
+                        uppercase === true &&
+                        lowercase === true &&
+                        number === true &&
+                        symbol === true
+                      ? 'green'
+                      : '',
+                }}
+              >
+                2
+              </p>
+              <p
+                className='border ml-1'
+                style={{
+                  backgroundColor:
+                    (length > 0 &&
+                      uppercase === true &&
+                      lowercase === true &&
+                      number === true &&
+                      symbol === false) ||
+                    (length > 0 &&
+                      uppercase === true &&
+                      lowercase === true &&
+                      number === false &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      uppercase === false &&
+                      lowercase === true &&
+                      number === true &&
+                      symbol === true) ||
+                    (length > 0 &&
+                      uppercase === true &&
+                      lowercase === false &&
+                      number === true &&
+                      symbol === true)
+                      ? 'yellow'
+                      : length > 0 &&
+                        uppercase === true &&
+                        lowercase === true &&
+                        number === true &&
+                        symbol === true
+                      ? 'green'
+                      : '',
+                }}
+              >
+                3
+              </p>
+              <p
+                className='border ml-1'
+                style={{
+                  backgroundColor:
+                    length > 0 &&
+                    uppercase === true &&
+                    lowercase === true &&
+                    number === true &&
+                    symbol === true
+                      ? 'green'
+                      : '',
+                }}
+              >
+                4
+              </p>
             </div>
           </section>
           <section className='mt-3 font-mono uppercase p-3 d-flex text-center sec-three font-semibold text-lg justify-center'>
             <p>generate</p>
-            <BsArrowRightShort className='arroe'/>
+            <BsArrowRightShort className='arroe' />
           </section>
         </main>
       </section>
